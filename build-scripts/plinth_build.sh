eae4be262b9dfddebdf785e6f577c541dd9745a8
Luojiaxing1991 <luojiaxing@huawei.com>
2018-05-03 16:45:16 +0800

    :sparkles: debug(1) plinth build script in jenkins
    plinth-ci.groovy:  call by jenkins job to start build kernel.
    
    plinth-ci.groovy:  make kernel

---
+++ build-scripts/plinth_build.sh
@@ -0,0 +1,119 @@
+#!/bin/bash -ex
+#: Title                  : jenkins_build_v500_start.sh
+#: Usage                  : ./local/ci-scripts/build-scripts/jenkins_build_v500_start.sh -p env.properties
+#: Author                 : qinsl0106@thundersoft.com
+#: Description            : CI中自动编译的jenkins任务脚本，针对v500
+# only works on centos
+
+#!/bin/bash
+
+PRE_TOP_DIR=$(cd "`dirname $0`" ; pwd)
+
+#KERNEL_GITADDR="https://github.com/hisilicon/kernel-dev.git"
+
+BUILD_DIR="/home/plinth"
+
+envok=0
+
+#****Check cmd support before running prepare actions for plinth test*****#
+
+#********
+#****Start : Clone kernel repo and build it
+#********
+
+#cd into the repo
+tmp=`echo ${KERNEL_GITADDR} | awk -F'.' '{print $2}' | awk -F'/' '{print $NF}'`
+echo "The name of kernel repo is "$tmp
+
+#checkout if build repo is exit or not!
+if [ ! -d "${BUILD_DIR}" ];then
+	mkdir ${BUILD_DIR}
+fi
+
+#checkout if build dir output dir is exit or not!
+if [ ! -d "${BUILD_DIR}/output" ];then
+	mkdir ${BUILD_DIR}/output
+fi
+
+#checkout if kernel repo is exit or not!
+if [ ! -d "${BUILD_DIR}/${tmp}" ];then
+	echo "The kernel dir is not exit! Begin to clone repo!"
+        cd ${BUILD_DIR}
+        #git clone ${KERNEL_GITADDR}
+
+	expect -c '
+		
+	set tmpaddr ${KERNEL_GITADDR}
+	
+	spawn git clone $tmpaddr
+	expect "Username for 'https://github.com':"
+	send "Luojiaxing1991\r"
+	expect "Password for 'https://Luojiaxing1991@github.com':"
+	send "ljxfyjh1321\r"
+	expect eof
+	exit 0
+	'
+else
+	echo "The kernel repo have been found!"
+fi
+
+cd ${BUILD_DIR}/${tmp}
+
+#generate the patch of pmu v2 to make perf support in D05
+git stash
+git checkout -b svm-4.15 remotes/origin/release-plinth-4.15.0
+tmp_patch=`git format-patch -1 b4e84aac21e48fcccc964216be5c7f8530db7b32`
+
+cp ${tmp_patch}  ${BUILD_DIR}/output
+
+#before checkout branch,update the remote branch list
+expect -c '
+spawn git remote update origin --prune
+expect "Username for 'https://github.com':"
+send "Luojiaxing1991\r"
+expect "Password for 'https://Luojiaxing1991@github.com':"
+send "ljxfyjh1321\r"
+expect eof
+exit 0
+'
+
+#git remote update origin --prune
+
+#checkout specified branch and build keinel
+git branch | grep ${BRANCH_NAME}
+git stash
+
+if [ $? -eq 0 ];then
+	#The same name of branch is exit
+	#git stash
+	git checkout -b tmp_luo origin/${BRANCH_NAME}
+	git branch -D ${BRANCH_NAME}
+fi
+
+git checkout -b ${BRANCH_NAME} origin/${BRANCH_NAME}
+git branch -D tmp_luo
+
+#before any change,patch the PMU patch to support D05
+git am --abort
+git am ${BUILD_DIR}/output/${tmp_patch}
+sleep 20
+git branch -D svm-4.15
+
+#before building,change some build cfg
+
+#HNS VLAN build option
+sed -i 's/CONFIG_VLAN_8021Q=m/CONFIG_VLAN_8021Q=y/g' arch/arm64/configs/defconfig
+
+#
+#sed -i '$a\CONFIG_IXGBE_DCB=y' arch/arm64/configs/plinth-config
+
+cat arch/arm64/configs/defconfig | grep  CONFIG_VLAN_8021Q
+
+echo "Begin to build the kernel!"
+bash build.sh ${BOARD_TYPE} > ${BUILD_DIR}/output/ok.log
+
+echo "Finish the kernel build!"
+
+#********
+#****END : Clone kernel repo and build it
+#********
---
+++ pipeline/plinth-ci.groovy
@@ -0,0 +1,106 @@
+def clone2local(giturl, branchname, localdir) {
+    def exists = fileExists localdir
+    if (!exists){
+        new File(localdir).mkdir()
+    }
+    dir (localdir) {
+        checkout([$class: 'GitSCM', branches: [[name: branchname]],
+                extensions: [[$class: 'CloneOption', timeout: 120]], gitTool: 'Default',
+                userRemoteConfigs: [[url: giturl]]
+            ])
+    }
+}
+
+def getGitBranchName() {
+    return scm.branches[0].name
+}
+
+def getGitUrl() {
+    return scm.getUserRemoteConfigs()[0].getUrl()
+}
+
+node ('ci-v500-compile'){
+
+
+    stage('Build Preparation') { // for display purposes
+        clone2local(getGitUrl(), getGitBranchName(), './local/ci-scripts')
+
+        // prepare variables.
+        sh 'env'
+
+        // save the properties
+        sh 'echo "" > env.properties'
+
+        // save jenkins enviroment properties.
+        sh "echo BUILD_URL=\\\"${BUILD_URL}\\\" >> env.properties"
+
+        // save jenkins parameters.
+		if (env.KERNEL_GITADDR) {
+            sh "echo KERNEL_GITADDR=\\\"${KERNEL_GITADDR}\\\" >> env.properties"
+        }
+		if (env.BRANCH_NAME) {
+            sh "echo BRANCH_NAME=\\\"${BRANCH_NAME}\\\" >> env.properties"
+        }
+		
+        if (env.TREE_NAME) {
+            sh "echo TREE_NAME=\\\"${TREE_NAME}\\\" >> env.properties"
+        }
+        if (env.BOOT_PLAN) {
+            sh "echo BOOT_PLAN=\\\"${BOOT_PLAN}\\\" >> env.properties"
+        }
+
+        if (env.SHELL_PLATFORM) {
+            sh "echo SHELL_PLATFORM=\\\"${SHELL_PLATFORM}\\\" >> env.properties"
+        }
+        if (env.SHELL_DISTRO) {
+            sh "echo SHELL_DISTRO=\\\"${SHELL_DISTRO}\\\" >> env.properties"
+        }
+
+        if (env.TEST_REPO) {
+            sh "echo TEST_REPO=\\\"${TEST_REPO}\\\" >> env.properties"
+        }
+        if (env.TEST_PLAN) {
+            sh "echo TEST_PLAN=\\\"${TEST_PLAN}\\\" >> env.properties"
+        }
+        if (env.TEST_SCOPE) {
+            sh "echo TEST_SCOPE=\\\"${TEST_SCOPE}\\\" >> env.properties"
+        }
+        if (env.TEST_LEVEL) {
+            sh "echo TEST_LEVEL=\\\"${TEST_LEVEL}\\\" >> env.properties"
+        }
+
+        if (env.SUCCESS_MAIL_LIST) {
+            sh "echo SUCCESS_MAIL_LIST=\\\"${SUCCESS_MAIL_LIST}\\\" >> env.properties"
+        }
+        if (env.SUCCESS_MAIL_CC_LIST) {
+            sh "echo SUCCESS_MAIL_CC_LIST=\\\"${SUCCESS_MAIL_CC_LIST}\\\" >> env.properties"
+        }
+        if (env.FAILED_MAIL_LIST) {
+            sh "echo FAILED_MAIL_LIST=\\\"${FAILED_MAIL_LIST}\\\" >> env.properties"
+        }
+        if (env.FAILED_MAIL_CC_LIST) {
+            sh "echo FAILED_MAIL_CC_LIST=\\\"${FAILED_MAIL_CC_LIST}\\\" >> env.properties"
+        }
+
+        if (env.DEBUG) {
+            sh "echo DEBUG=\\\"${DEBUG}\\\" >> env.properties"
+        }
+    }
+    // load functions
+    def functions = load "./local/ci-scripts/pipeline/functions.groovy"
+
+
+    def build_result = 0
+    stage('Build') {
+        build_result = sh script: "./local/ci-scripts/build-scripts/plinth_build.sh", returnStatus: true
+    }
+    echo "build_result : ${build_result}"
+    if (build_result == 0) {
+        echo "build success"
+    } else {
+        echo "build failed"
+        functions.send_mail()
+        currentBuild.result = 'FAILURE'
+        return
+    }
+}
