efcf6065d86f2ddf791376423393e6caebe563ac
Luojiaxing1991 <luojiaxing@huawei.com>
2018-05-05 21:37:20 +0800

    debug 40

--- build-scripts/gitclone.sh
+++ build-scripts/gitclone.sh
@@ -10,11 +10,11 @@ set timeout 120
 expect {
 	"Username"	{send "Luojiaxing1991\r";exp_continue}
 	"Password"	{send "ljxfyjh1321\r";exp_continue}
-	"remote"	{exp_continue}
-	"Receiving"	{exp_continue}
-	"Resolving"	{exp_continue}
-	"Checking connectivity"	{exp_continue}
-	"Checking out"	{exp_continue}
+#	"remote"	{exp_continue}
+#	"Receiving"	{exp_continue}
+#	"Resolving"	{exp_continue}
+#	"Checking connectivity"	{exp_continue}
+#	"Checking out"	{exp_continue}
 	default		{exp_continue}
 }
 
