d09a99ccd3da680ff6d835a4d3727b252a6f7d33
Luojiaxing1991 <luojiaxing@huawei.com>
2018-05-13 09:18:55 +0800

    debug 55

--- pipeline/plinth-ci.groovy
+++ pipeline/plinth-ci.groovy
@@ -94,7 +94,8 @@ node ('ci-compile'){
 
     def build_result = 0
     stage('Build') {
-        build_result = sh script: "./local/ci-scripts/build-scripts/plinth_build.sh", returnStatus: true
+        //build_result = sh script: "./local/ci-scripts/build-scripts/plinth_build.sh", returnStatus: true
+	build_result = 1
     }
     echo "build_result : ${build_result}"
     if (build_result == 0) {
