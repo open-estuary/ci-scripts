9ff228402ec8b309630265044305a9ba3a804173
Luojiaxing1991 <luojiaxing@huawei.com>
2018-05-15 18:04:32 +0800

    debug salil 3

--- build-scripts/plinth_build.sh
+++ build-scripts/plinth_build.sh
@@ -199,9 +199,16 @@ git pull
 #sed -i 's/CONFIG_HNS3_HCLGE=m/CONFIG_HNS3_HCLGE=y/g' arch/arm64/configs/plinth-config
 #sed -i 's/CONFIG_HNS3_ENET=m/CONFIG_HNS3_ENET=y/g' arch/arm64/configs/plinth-config
 
+[ ! -f  'arch/arm64/configs/defconfig' ] && echo " " > arch/arm64/configs/defconfig 
+
+sed -i 's/CONFIG_HNS3=m/CONFIG_HNS3=y/g' arch/arm64/configs/defconfig
+sed -i 's/CONFIG_HNS3_HCLGE=m/CONFIG_HNS3_HCLGE=y/g' arch/arm64/configs/defconfig
+sed -i 's/CONFIG_HNS3_ENET=m/CONFIG_HNS3_ENET=y/g' arch/arm64/configs/defconfig
+
+
 
 #HNS VLAN build option
-#sed -i 's/CONFIG_VLAN_8021Q=m/CONFIG_VLAN_8021Q=y/g' arch/arm64/configs/defconfig
+sed -i 's/CONFIG_VLAN_8021Q=m/CONFIG_VLAN_8021Q=y/g' arch/arm64/configs/defconfig
 
 
 
