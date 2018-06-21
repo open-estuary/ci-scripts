69eccd7b44846388a04dffc0d1f350f3b2070fc8
Luojiaxing1991 <luojiaxing@huawei.com>
2018-06-21 09:23:02 +0800

    :memo: debug 1

--- test_python/test.py
+++ test_python/test.py
@@ -2,11 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import code as func
+import pexpect
 
-func.generate_test_report(123)
+uefi = pexpect.spawn("ssh root@192.168.3.159")
+logfile = open("logfile.txt",'w')
+uefi.logfile = logfile
+uefi.expect(["password",pexpect.EOF],timeout=100)
+#uefi.sendline("\13")
+uefi.sendcontrol('M')
+#func.generate_test_report(123)
 
 #func.generate_email_test_report()
 
-func.generate_email_locate_report()
-
-
+#func.generate_email_locate_report
